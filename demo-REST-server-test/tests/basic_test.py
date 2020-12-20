08c24619c97c2a830fafff26d9eaa0818255cc87
Cezuriku <cezuriku@riseup.net>
2020-12-20 17:08:11 +0100

    Add basic succeed tests

--- demo-REST-server-test/tests/basic_test.py
+++ demo-REST-server-test/tests/basic_test.py
@@ -1,10 +1,40 @@
-from helpers import Server
+from helpers import Server, is_success
 import os
+import pytest
+
+### Fixtures ###
 
 
-# Not in the specifications
-def test_put_request_returns_201():
-    server = Server("http://" + os.getenv("SERVER_URL"))
+@pytest.fixture(scope='function')
+def server():
+    """Return a server and clean it when the test is done"""
+    s = Server("http://" + os.getenv("SERVER_URL"))
+    yield s
+    s.clean()
+
+### Tests ###
+
+
+def test_put_request_succeed(server):
+    assert is_success(server.put("/hello/world", {"answer": 42}))
+
+
+def test_put_request_twice_succeed(server):
     path = "/hello/world"
     data = {"answer": 42}
-    assert server.put(path, data).status_code == 201
+    server.put(path, data)
+    assert is_success(server.put(path, data))
+
+
+def test_get_request_succeed(server):
+    path = "/hello/world"
+    data = {"answer": 42}
+    server.put(path, data)
+    assert is_success(server.get(path))
+
+
+def test_delete_request_succeed(server):
+    path = "/hello/world"
+    data = {"answer": 42}
+    server.put(path, data)
+    assert is_success(server.delete(path))
--- demo-REST-server-test/tests/helpers.py
+++ demo-REST-server-test/tests/helpers.py
@@ -1,18 +1,37 @@
 import requests
 
 
+def is_success(response: requests.Response):
+    """Return true if the response has a success HTTP status code"""
+    return 200 <= response.status_code < 300
+
+
 class Server:
     def __init__(self, base_uri):
         self.base_uri = base_uri
+        self._data = {}
 
-    def put(self, path, data):
-        """Perform a put request to the server and return the result"""
-        return requests.put(self.base_uri + path, json=data)
+    def put(self, path: str, data):
+        """Perform a put request to the server and return the response"""
+        response = requests.put(self.base_uri + path, json=data)
+        if response:
+            self._data[path] = data
+        return response
 
     def get(self, path):
-        """Perform a get request to the server and return the result"""
+        """Perform a get request to the server and return the response"""
         return requests.get(self.base_uri + path)
 
     def delete(self, path):
-        """Perform a delete request to the server and return the result"""
-        return requests.delete(self.base_uri + path)
+        """Perform a delete request to the server and return the response"""
+        response = requests.delete(self.base_uri + path)
+        try:
+            del self._data[path]
+        except KeyError:
+            pass
+        return response
+
+    def clean(self):
+        for path in self._data.keys():
+            requests.delete(self.base_uri + path)
+        self._data = {}
